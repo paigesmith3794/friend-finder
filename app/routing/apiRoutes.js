2fb2ef69e95cd03c50ac37a0197e0e87189cd6db
paigesmith3794 <paigesmith3794@gmail.com>
2019-06-14 19:43:39 -0700

    working on apiRoutes.js file

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,39 @@
+var friends = require("../data/friends.js");
+
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    });
+
+    app.post("/api/friends", function (req, res) {
+        var totalDifference = 0;
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        };
+        var userData = req.body;
+        var useName = userData.name;
+        var userScores = userData.scores;
+
+        var b = userScores.map(function (item) {
+            return parseInt(item, 10);
+        });
+        userData = {
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: b
+        };
+
+        console.log("Name: " + userName);
+        console.log("User score " + userScores);
+
+        var sum = b.reduce((a, b) => a + b, 0);
+        console.log("Sum of users score " + sum);
+        console.log("Best match friend difference " + bestMatch.friendDifference);
+        console.log("==========================================================");
+
+
+    });
+};
+
